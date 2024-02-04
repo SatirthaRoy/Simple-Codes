3413541bf9f34cb10e84046c899112a663f1ff23
himu <satirtharoy2003@gmail.com>
2024-02-04 18:15:07 +0600

    added remove dupli and capitalize first letter

--- index.js
+++ index.js
@@ -2,6 +2,16 @@ const simpleCodes = {
   isString(s) {
     return typeof s === 'string';
   },
+  capitalizeFirstLetter(s) {
+    const a = s.split(' ');
+    const b = a.map(word => {
+      return word.replace(word[0], word[0].toUpperCase());
+    });
+    return b.join(' ');
+  },
+  removeDuplicates(a) {
+    return a.filter((item,i)=>a.lastIndexOf(item)==i);
+  }
 };
 
-export default simpleCodes;
+module.exports = simpleCodes;

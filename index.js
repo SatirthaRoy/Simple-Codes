0db9ccb9b3f9f0001477f0d375272bd9f0df8ce3
himu <satirtharoy2003@gmail.com>
2024-02-04 21:11:35 +0600

    added error handling

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ const simpleCodes = {
   },
 
   capitalizeFirstLetter(s) {
+    if(typeof s !== 'string') {
+      return 'please provide string';
+    }
     const a = s.split(' ');
     const b = a.map(word => {
       return word.replace(word[0], word[0].toUpperCase());
@@ -12,22 +15,34 @@ const simpleCodes = {
   },
 
   removeDuplicates(a) {
+    if(!Array.isArray(a)) {
+      return 'please provide an array';
+    }
     return a.filter((item,i)=>a.lastIndexOf(item)==i);
   },
 
   sortAscend(a) {
+    if(!Array.isArray(a)) {
+      return 'please provide an array';
+    }
     return a.sort((a,b)=> a-b);
   },
   
   sortDescend(a) {
+    if(!Array.isArray(a)) {
+      return 'please provide an array';
+    }
     return a.sort((a,b)=> b-a);
   },
 
   sumOfAllNums(a) {
+    if(!Array.isArray(a)) {
+      return 'please provide an array';
+    }
     return a.reduce((initial, value) => initial + value,0);
   }
 };
 
-console.log(simpleCodes.sumOfAllNums([1,2,3,4,5]));
+//console.log(simpleCodes.sumOfAllNums([1,2,3,4,5]));
 
 module.exports = simpleCodes;

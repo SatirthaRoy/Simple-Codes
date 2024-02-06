cb5c25ed93d8dab415b3b0b889ffa9a816dd83cc
himu <satirtharoy2003@gmail.com>
2024-02-06 12:12:18 +0600

    added returnMax/Min , isEven/Odd function

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const simpleCodes = {
+  // returns true/false
   isString(s) {
     return typeof s === 'string';
   },
 
+  //capitalize first letter of each word in a sentence
   capitalizeFirstLetter(s) {
     if(typeof s !== 'string') {
       return 'please provide string';
@@ -14,6 +16,7 @@ const simpleCodes = {
     return b.join(' ');
   },
 
+  // remove duplicates in an array
   removeDuplicates(a) {
     if(!Array.isArray(a)) {
       return 'please provide an array';
@@ -21,6 +24,7 @@ const simpleCodes = {
     return a.filter((item,i)=>a.lastIndexOf(item)==i);
   },
 
+  // sorts an array of number in ascending order
   sortAscend(a) {
     if(!Array.isArray(a)) {
       return 'please provide an array';
@@ -28,6 +32,7 @@ const simpleCodes = {
     return a.sort((a,b)=> a-b);
   },
   
+  // sorts an array of number in descending order
   sortDescend(a) {
     if(!Array.isArray(a)) {
       return 'please provide an array';
@@ -35,14 +40,48 @@ const simpleCodes = {
     return a.sort((a,b)=> b-a);
   },
 
+
+  // sum of all numbers in an array
   sumOfAllNums(a) {
     if(!Array.isArray(a)) {
       return 'please provide an array';
     }
     return a.reduce((initial, value) => initial + value,0);
-  }
+  },
+
+  // checks if even
+  isEven(n) {
+    if(typeof n !== 'number') {
+      return 'please provide a number';
+    }
+    return n%2 == 0;
+  },
+
+  // checks if odd
+  isOdd(n) {
+    if(typeof n !== 'number') {
+      return 'please provide a number';
+    }
+    return n%2 == 1;
+  },
+
+  // returns the maximum number in an array
+  returnMax(a) {
+    if(!Array.isArray(a)) {
+      return 'please provide an array';
+    }
+    return a.sort((a,b) => b-a)[0];
+  },
+
+  // returns the minimum number in an array
+  returnMin(a) {
+    if(!Array.isArray(a)) {
+      return 'please provide an array';
+    }
+    return a.sort((a,b) => a-b)[0];
+  },
 };
 
-//console.log(simpleCodes.sumOfAllNums([1,2,3,4,5]));
+console.log(simpleCodes.returnMin([5,5,2,89,32,19]));
 
 module.exports = simpleCodes;

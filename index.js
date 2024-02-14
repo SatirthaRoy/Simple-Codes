5850d2b8abc940f4625ebc6deab85ce5fd9fd108
himu <satirtharoy2003@gmail.com>
2024-02-14 17:10:36 +0600

    new feature calculate

--- index.js
+++ index.js
@@ -147,91 +147,11 @@ const simpleCodes = {
     if(isNaN(Number(a[a.length-1]))) {
       return 'Invalid Expression';
     }
-  
-    // splits the numbers and operators
-    function splitNumbersOp (a) {
-      let arr = [];
-      let w ='';
-      a.forEach((item,i) => {
-        w += item;
-        if(isNaN(Number(a[i+1])) && a[i+1] !== '.') {
-          arr.push(String(Number(w)));
-          w='';
-        }
-        if(isNaN(Number(item)) && item !== '.') {
-          arr.push(w);
-          w='';
-        }
-      });
-      return arr;
-    }
-  
-    let arr = splitNumbersOp(a);
-  
-    // mathematics starts here
-    // checks division first
-    let str = arr.join('');
-    while(str.includes('/')) {
-      let num1 = Number(arr[arr.lastIndexOf('/')-1]);
-      let num2 = Number(arr[arr.lastIndexOf('/')+1]);
-      str = str.replaceAll(`${num1}/${num2}`, `${num1/num2}`);
-      a = str.split('');
-      arr = splitNumbersOp(a);
-    }
-    //console.log(str);
-    //checks multiplication second
-    while(str.includes('*')) {
-      let num1 = Number(arr[arr.lastIndexOf('*')-1]);
-      let num2 = Number(arr[arr.lastIndexOf('*')+1]);
-      str = str.replaceAll(`${num1}*${num2}`, `${num1*num2}`);
-      a = str.split('');
-      arr = splitNumbersOp(a);
-      console.log(arr);
-      console.log(str);
-    }
-    console.log(str);
-  
-    // checks addition third
-    while(str.includes('+')) {
-      let num1 = arr[arr.lastIndexOf('+')-2] === '-' ? Number(-arr[arr.lastIndexOf('+')-1]) : Number(arr[arr.lastIndexOf('+')-1]);
-      let num2 = Number(arr[arr.lastIndexOf('+')+1]);
-      let sign;
-      if(arr[arr.lastIndexOf('+')-2] === '+') {
-        sign = '+';
-      } else if (arr[arr.lastIndexOf('+')-2] === '-') {
-        sign = '-';
-      }else {
-        sign = '';
-      }
-  
-      if((num1 + num2) > 0) {
-        str = str.replaceAll(`${sign}${Math.abs(num1)}+${num2}`, `${num1+num2 === 0 ? '' : sign!== '' ? `+${num1+num2}` : num1+num2}`)
-      }
-      if((num1 + num2) <= 0) {
-        str = str.replaceAll(`${sign}${Math.abs(num1)}+${num2}`, `${num1+num2 === 0 ? '0' : num1+num2}`);
-      }
-      if(arr.lastIndexOf('+') == 0) {
-        str = str.replaceAll('+','');
-      }
-      a = str.split('');
-      arr = splitNumbersOp(a);
-    }
-    //console.log(str);
-  
-    // checks substraction last
-    while(str.includes('-') && arr.lastIndexOf('-') !== 0) {
-      let num1 = arr[arr.lastIndexOf('-')-2] === '-' ? Number(-arr[arr.lastIndexOf('-')-1]) : Number(arr[arr.lastIndexOf('-')-1]);
-      let num2 = Number(arr[arr.lastIndexOf('-')+1]);
-      str = str.replaceAll(`${num1}-${num2}`, `${num1-num2}`);
-      a = str.split('');
-      arr = splitNumbersOp(a);
-    }
-    //console.log(str);
-    
-    return str;
+    let str = a.join('');
+    return eval(str);
   }, 
 };
 
-//console.log(simpleCodes.calculate('4.50*2'));
+//console.log(simpleCodes.calculate('1-1+10+10-20'));
 
 module.exports = simpleCodes;
